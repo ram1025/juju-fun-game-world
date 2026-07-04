4fc6304bffbddb2f114286f59abb53fc2d75dca1
ram1025 <87440237+ram1025@users.noreply.github.com>
2026-07-04 15:05:36 +0530

    Increment cache version number to v65
    Updated cache version number from v64 to v65.

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'juju-fun-world-v64'; // <-- Ikkada v number marchu
+const CACHE_NAME = 'juju-fun-world-v65'; // <-- Ikkada v number marchu
 const urlsToCache = [
     './',
     './index.html',
