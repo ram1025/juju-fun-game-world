fd5104aeaead9de742d84b987e8cf287d4b658d7
ram1025 <87440237+ram1025@users.noreply.github.com>
2026-07-04 16:10:06 +0530

    Update CACHE_NAME version to v68

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'juju-fun-world-v67'; // <-- Ikkada v number marchu
+const CACHE_NAME = 'juju-fun-world-v68'; // <-- Ikkada v number marchu
 const urlsToCache = [
     './',
     './index.html',
