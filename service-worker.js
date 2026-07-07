d9bea4b7b183f634280475c4772789afb10282f2
ram1025 <87440237+ram1025@users.noreply.github.com>
2026-07-07 20:44:30 +0530

    Change CACHE_NAME to 'juju-fun-world-v11-3'
    Updated cache name to version 11-3.

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'juju-fun-world-v11-2'; // 🔥 V11 nunchi V11-1 ki marcham. Shape Studio Voice Add
+const CACHE_NAME = 'juju-fun-world-v11-3'; // 🔥 V11 nunchi V11-1 ki marcham. Shape Studio Voice Add
 
 const urlsToCache = [
     './',
