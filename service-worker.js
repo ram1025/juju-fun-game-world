8c3f61f05949b061300bb6e0cc8757550c83c665
ram1025 <87440237+ram1025@users.noreply.github.com>
2026-08-03 19:30:58 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,44 +1,44 @@
-const CACHE_NAME = 'juju-v111-cache-first';
+const CACHE_NAME = 'juju-v112-01-cache-first';
 const urlsToCache = [
   './',
   './index.html',
   './dashboard.html',
   './profile.html',
-  './manifest.json?v=111',
+  './manifest.json?v=112-01',
   './icon-192.png',
   './icon-512.png',
   './style.css',
   // ALL GAMES
-  './addition.html',
-  './alphabets.html',
-  './balloon-pop-edu.html',
+  './addition.html', 
+  './alphabets.html', 
+  './balloon-pop-edu.html', 
   './body-parts.html',
-  './juju-car-race.html',
-  './color-mixing.html',
-  './colors.html',
+  './juju-car-race.html', 
+  './color-mixing.html', 
+  './colors.html', 
   './drag-drop.html',
-  './free-draw.html',
-  './fruits.html',
-  './matching.html',
+  './free-draw.html', 
+  './fruits.html', 
+  './matching.html', 
   './numbers.html',
-  './painting.html',
-  './pet-animals.html',
-  './rhymes.html',
+  './painting.html', 
+  './pet-animals.html', 
+  './rhymes.html', 
   './shapes.html',
-  './spell-it.html',
-  './temple-run.html',
-  './vehicles.html',
+  './spell-it.html', 
+  './temple-run.html', 
+  './vehicles.html', 
   './vegetables.html',
-  './wild-animals.html',
+  './wild-animals.html', 
   './connect-dots.html',
-  // ALL 8 RHYMES - SPELLING FIXED
-  './abc-rhyme.mp3',
-  './baa-baa-black.mp3',
-  './humpty-dumpty.mp3', // FIXED: dummpy-dumpty -> dumpty
-  './johnny-johnny.mp3',
-  './ring-ring-roses.mp3',
+  // ALL 8 RHYMES
+  './abc-rhyme.mp3', 
+  './baa-baa-black.mp3', 
+  './humpty-dumpty.mp3', 
+  './johnny-johnny.mp3', 
+  './ring-ring-roses.mp3', 
   './twinkle-twinkle.mp3',
-  './wheels-on-bus.mp3',
+  './wheels-on-bus.mp3', 
   './you-are-my-sunshine.mp3'
 ];
 
@@ -68,7 +68,6 @@ self.addEventListener('activate', e => {
 self.addEventListener('fetch', e => {
   const url = e.request.url;
 
-  // MP3 FILES - Cache first
   if(url.endsWith('.mp3')){
     e.respondWith(
       caches.match(e.request).then(cached => {
@@ -83,7 +82,6 @@ self.addEventListener('fetch', e => {
     return;
   }
 
-  // OTHER FILES - Cache first
   e.respondWith(
     caches.match(e.request).then(res => {
       return res || fetch(e.request).catch(() => {
